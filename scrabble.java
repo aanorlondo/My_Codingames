21666db8673577302c7100774d6aca6d9a337144
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2021-11-06 14:26:55 +0100

    Update scrabble.java
    https://www.codingame.com/ide/puzzle/scrabble

--- scrabble.java
+++ scrabble.java
@@ -1,14 +1,86 @@
 import java.util.*;
-import java.io.*;
-import java.math.*;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+class Letter { //a structure for {char,incidence}
+    char c;
+    int nb;
+    public Letter(char c) {
+        this.c = c; this.nb = 1;
+    }
+}
+
+class Word { //words to play with their position (in the dic) and points
+    String w;
+    int points;
+    int position;
+    public Word(String w, int pos) {
+        this.w = w;
+        this.position = pos;
+    }
+}
+
 class Solution {
 
-    public static int score(String word) { //calculating points for a word
+    /*building a set of {char, incidence} to build the playable words later */
+    public static Set<Letter> buildSet(String chars) {
+        Set<Letter> set = new HashSet<>();
+        for (int i = 0; i < chars.length() ; i++)
+        {
+            Boolean exist = false;
+            for(Letter l : set) {
+                if (l.c == chars.charAt(i)) {
+                    l.nb++;
+                    exist = true;
+                }
+            }
+            if (!exist) {
+                set.add(new Letter(chars.charAt(i)));
+            }
+        }
+        return set;
+    }
+
+    public static boolean include(Set<Letter> set, char c) {
+        for (Letter l : set) {
+            if (l.c == c && l.nb > 0) return true;
+        }
+        return false;
+    }
+
+    /*same as above, but can be used to decrease the incidence or not*/
+    public static boolean include(Set<Letter> set, char c, boolean dec) {
+        for (Letter l : set) {
+            if (l.c == c && l.nb > 0) {
+                if (dec) 
+                    l.nb--;
+                return true;        
+            }
+        }
+        return false;
+    }
+
+    /*pick from the dictionnary the words that can be played from the set of letters*/
+    public static Set<Word> filter(String chars, Set<Word> dico) {
+        Set<Word> set = new HashSet<>();
+        for (Word s : dico) {
+            Set<Letter> letters = buildSet(chars);
+            boolean playable = true;
+            Word word = s;
+            for (int i = 0 ; i < s.w.length() && playable ; i ++)
+            {
+                if (!include(letters,s.w.charAt(i),true)) 
+                    playable = false;
+            }
+            if (playable) {
+                word.points = points(word.w);
+                set.add(word);
+            }
+            letters.clear();
+        }
+        return set;
+    }
+
+    /*calculating points for a word*/
+    public static int points(String word) { 
         String row1 = "1eaionrtlsu";
         String row2 = "2dg";
         String row3 = "3bcmp";
@@ -28,37 +100,49 @@ class Solution {
             if ( row7.indexOf(word.charAt(i)) != -1) score += Integer.parseInt(String.valueOf(row7.charAt(0))) * 10;
         }
         return score;
-    } 
-
-    public static Set<String> permute(String chars) {
-    /*SOURCE: https://stackoverflow.com/questions/9666903/every-combination-of-character-array*/
-        Set<String> set = new HashSet<>();
-        // Termination condition: only 1 permutation for a string of length 1
-        if (chars.length() == 1)
-        {
-            set.add(chars);
-        }
-        else
-        {
-            // Give each character a chance to be the first in the permuted string
-            for (int i=0; i<chars.length(); i++)
-            {
-                // Remove the character at index i from the string
-                String pre = chars.substring(0, i);
-                String post = chars.substring(i+1);
-                String remaining = pre+post;
-
-                // Recurse to find all the permutations of the remaining chars
-                for (String permutation : permute(remaining))
-                {
-                    // Concatenate the first character with the permutations of the remaining chars
-                    set.add(chars.charAt(i) + permutation);
-                }
-            }
-        }
-        return set;
     }
 
+    /*getting the highest score from a words set*/
+    public static int max(Set<Word> set) {
+        int max = 0;
+        for (Word w : set) {
+            if (w.points > max) 
+                max = w.points;
+        }
+        return max;
+    }
+
+    /*sorting the words to keep only those with best points*/
+    public static Set<Word> pick(Set<Word> set) {
+        int max = max(set);
+        Set<Word> Best = new HashSet<>();
+        for (Word word : set) {
+            if (word.points >= max) {
+                Best.add(word);
+            }
+        }
+        return Best;
+    }
+
+    /*clean the set of possibilities to choose the first word chronologically*/
+    public static Word play(Set<Word> set) {
+        Word min = new Word("",0);
+        if (set.iterator().hasNext()) {
+            min = set.iterator().next();
+            if (set.size() == 1) 
+                return set.iterator().next();
+        }
+        System.err.println("Words being sorted :");
+        for (Word word : set) {
+            System.err.println("\t"+word.w);
+            if (word.position <= min.position) {//lexicographically precedes or equals 
+                min = word;
+            }
+        }
+        return min;
+    }
+
+    /*********MAIN*****************/
     public static void main(String args[]) {
         
         /*The inputs*/
@@ -67,46 +151,53 @@ class Solution {
         if (in.hasNextLine()) {
             in.nextLine();
         }
-        Set<String> Dico = new HashSet<>();
+        Set<Word> Dico = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            String w = (in.nextLine());
-            Dico.add (w); //add words to dico
+            //adding words to dictionnary
+            Dico.add( new Word(  in.nextLine() , i+1));
         }
-        String chars = in.nextLine(); //getting the letters to play
+        String Chars = in.nextLine(); //getting the letters to play
         in.close();
 
         
         /*Print Dictionnary*/
-        System.err.println("Dico : ");
-        for (String s : Dico) {
-            System.err.println("\t"+s); 
+        System.err.println("Dico (position) : ");
+        for (Word s : Dico) {
+            System.err.println("\t"+s.w+" ("+s.position+")"); 
         }
 
         /*Print letters to play*/
         System.err.println("Letters : ");
-        System.err.println("\t"+chars); 
+        System.err.println("\t"+Chars); 
 
-        /*Generating all possible words from these letters*/
-        Set<String> Words = permute(chars); //A REFAIRE
-        Set<String> Clean = new HashSet<>();
-
-        /*clean words to keep only the ones matched in the dictionary*/
-        for (String s : Words) {
-            if (Dico.contains(s)) {
-				Clean.add(s);
-            }                                                
+        /*The set of words than can be played*/
+        Set<Word> Words = filter(Chars,Dico);
+        System.err.println("Playable words (points) :");
+        for (Word w : Words) {
+            System.err.println("\t"+w.w+" ("+w.points+")");
         }
 
-        /*choosing the word to play*/
-        int points = 0;
-        String output= "";
-        for (String s : Clean) { 
-            if (score(s) > points) {
-                points = score(s); 
-                output = s;
+        Word output;
+        Set<Word> Best;
+        /*Filter to pick ones with the best points (if needed)*/
+        if (Words.size() > 1) {
+            Best = pick(Words);
+                    System.err.println("Best words to play:");
+            for (Word w : Best) {
+                System.err.println("\t"+w.w+" ("+w.points+")");
             }
+            output = play(Best);
+            Best.clear();
+        } else {
+            output = Words.iterator().next();
         }
-        System.err.println("With "+points+" points, the best word to play is : ");
-        System.out.println(output);
+
+        /*Some output*/
+        System.err.println("With "+output.points+" points, the best word to play is : ");
+        System.out.println(output.w);
+        
+        /*free some space*/
+        Dico.clear(); 
+        Words.clear();
     }
 }
