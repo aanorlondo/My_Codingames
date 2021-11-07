19127ed994d1e763b3e4a661a7871640d1256dca
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2021-11-07 01:33:56 +0100

    Update sudoko-moreJSish.js

--- sudoko-moreJSish.js
+++ sudoko-moreJSish.js
@@ -2,9 +2,9 @@ function hasDuplicates(array) {
     return (new Set(array)).size !== array.length;
 }
 
-function getColumn(anArray, columnNumber) {
-    return anArray.map(function(row) {
-        return row[columnNumber];
+function getColumn(array, col) {
+    return array.map(function(row) {
+        return row[col];
     });
 }
 
@@ -13,8 +13,7 @@ for (let i = 0; i < 9; i++) {
     var inputs = readline().split(' ');
     grille[i] = new Array(9);
     for (let j = 0; j < 9; j++) {
-        const n = parseInt(inputs[j]);
-        grille[i][j] = n;
+        grille[i][j] = parseInt(inputs[j]);
     }
 }
 //console.error('SUDOKO VALIDATOR');
