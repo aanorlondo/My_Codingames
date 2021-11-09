4557fbbfcf627de7b8aa98557b78643f23affd89
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2021-11-09 09:15:16 +0100

    Update 1Dspreadsheet-memoize.js

--- 1Dspreadsheet-memoize.js
+++ 1Dspreadsheet-memoize.js
@@ -1,31 +1,32 @@
 function evaluate(n, memo) {
     memo = memo || {};
-    console.error('evaluating line',n);
-    if (memo[n]) { 
-        console.error('line',n,'value already known');
+        console.error('evaluating line',n);
+        if (n in memo) { 
+            console.error('line',n,'value already known');
+            return memo[n];
+        }
+        else if (lines[n][0] == 'VALUE') {
+            console.error('\treading VALUE');
+            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1)));
+        }
+        else if  (lines[n][0] == 'ADD') {
+            console.error('\treading ADD');
+            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
+            memo[n] +=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(1)));
+        }
+        else if  (lines[n][0] == 'SUB') {
+            console.error('\treading SUB');
+            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
+            memo[n] -=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(1)));
+        }
+        else if  (lines[n][0] == 'MULT') {
+            console.error('\treading MULT');
+            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
+            memo[n] *=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(1)));
+        }
+        console.error('\tline',n,'evaluated at',memo[n]);
+        if (memo[n] == -0) memo[n] = 0 ;
         return memo[n];
-    }
-    else if (lines[n][0] == 'VALUE') {
-        console.error('\treading VALUE');
-        memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1)));
-    }
-    else if  (lines[n][0] == 'ADD') {
-        console.error('\treading ADD');
-        memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
-        memo[n] +=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(2)));
-    }
-    else if  (lines[n][0] == 'SUB') {
-        console.error('\treading SUB');
-        memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
-        memo[n] -=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(2)));
-    }
-     else if  (lines[n][0] == 'MULT') {
-        console.error('\treading MULT');
-        memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
-        memo[n] *=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(2)));
-    }
-    console.error('\tline',n,'evaluated at',memo[n]);
-    return memo[n];
 }
 
 const N = parseInt(readline());
