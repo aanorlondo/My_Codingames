dd4026ed37fa9802c63c561fb861684bcb572d45
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2021-11-09 09:12:32 +0100

    Update 1Dspreadsheet-cache.js

--- 1Dspreadsheet-cache.js
+++ 1Dspreadsheet-cache.js
@@ -1,31 +1,32 @@
 const memeval = n => {
-    const memo = []; const evaluate = n => {
+    const cache = []; const evaluate = n => {
         console.error('evaluating line',n);
-        if (n in memo) { 
+        if (n in cache) { 
             console.error('line',n,'value already known');
-            return memo[n];
+            return cache[n];
         }
         else if (lines[n][0] == 'VALUE') {
             console.error('\treading VALUE');
-            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1)));
+            cache[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1)));
         }
         else if  (lines[n][0] == 'ADD') {
             console.error('\treading ADD');
-            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
-            memo[n] +=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(2)));
+            cache[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
+            cache[n] +=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(1)));
         }
         else if  (lines[n][0] == 'SUB') {
             console.error('\treading SUB');
-            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
-            memo[n] -=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(2)));
+            cache[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
+            cache[n] -=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(1)));
         }
         else if  (lines[n][0] == 'MULT') {
             console.error('\treading MULT');
-            memo[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
-            memo[n] *=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(2)));
+            cache[n] = (!lines[n][1].includes('$')) ? parseInt(lines[n][1]) : evaluate(parseInt(lines[n][1].substring(1))); 
+            cache[n] *=(!lines[n][2].includes('$')) ? parseInt(lines[n][2]) : evaluate(parseInt(lines[n][2].substring(1)));
         }
-        console.error('\tline',n,'evaluated at',memo[n]);
-        return memo[n];
+        console.error('\tline',n,'evaluated at',cache[n]);
+        if (cache[n] == -0) cache[n] = 0 ;
+        return cache[n];
     }; return evaluate(n);
 };
 
@@ -38,6 +39,7 @@ for (let i = 0; i < N; i++) {
     lines[i][1] = inputs[1];
     lines[i][2] = inputs[2];
 }
+//console.table(lines);
 for (let i = 0; i < N; i++) {
     console.log(memeval(i));
 }
