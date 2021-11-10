ddb51cb023d1a0ff0bf7feb9a6d88ac5aeccd709
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2021-11-11 00:40:41 +0100

    Update ludo.py

--- ludo.py
+++ ludo.py
@@ -1,10 +1,15 @@
-p=int(input())
-t=[0,0]
-r=1
-for c in zip(input()) :
-    if(c=='6'):
-        t[r-1]+=1
-        if(t[r-1]==2):
-            print(r) 
-    else:r+=1%r
-print(0)
+p=3 //int(input())
+t=[]
+for i in range (p): 
+	t.append(0)
+r=0
+for c in ("64658662138"): //input()
+    print("Player",r+1,"plays",c)
+    if (c=='6') :
+        t[r]+=1
+        print("\tPlayer",r+1,"total pawns on  table",t[r])
+        if(t[r]==2):
+            print("\tPlayer",r+1,"wins !!!!")
+            quit()
+    else: r=(r+1)%p 
+print("No one wins")
