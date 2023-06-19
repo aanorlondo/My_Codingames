a693419daea278ae53395cc8a2ae800698c4baf5
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2023-06-19 23:32:12 +0200

    Update merlins-magic-square.js

--- merlins-magic-square.js
+++ merlins-magic-square.js
@@ -11,9 +11,9 @@ function reverse(button: char):char {
 }
 
 // reverse a row (3 buttons)
-function reverse_row(row: array): array{
-    for (i = 0; i < 2; i++) {
-        row[i] = reverse(row[i]);
+function reverse_row(row: array, size: int): array{
+    for (i = 0; i <= size; i++) {
+        row[i] = reverse(button=row[i]);
     }
     return row;
 }
@@ -21,7 +21,7 @@ function reverse_row(row: array): array{
 // reverse a square (2 x 2 buttons)
 function reverse_square(square: array): array{
     for (i = 0; i < 2; i++) {
-        square[i] = reverse_row(square[i]);
+        square[i] = reverse_row(row=square[i], size=2);
     }
     return square;
 }
@@ -29,19 +29,28 @@ function reverse_square(square: array): array{
 function transform(matrix: array, button:char):array {
     let button = parseInt(button);
 
+    // center
     if (button == 5) {
         matrix[1][1] = reverse(matrix[1][1]);
     }
-    return matrix;
     
-    if (button in [1, 3, 7, 9]) {
-        
+    // corners
+    else if (button in [1, 3, 7, 9]) {
+        // 1 --> 00 01 10 11
+        // 3 --> 01 02 11 12
+        // 7 --> 01 11 20 21
+        // 9 --> 11 12 21 22
     }
     
-    if (button in [2, 4, 6, 8]) {
+    // sides
+    else if (button in [2, 4, 6, 8]) {
+        // 2 --> 00 01 02
+        // 4 --> 00 10 20
+        // 6 --> 02 12 22
+        // 8 --> 20 21 22
+    }
 
-    }
-    
+    return matrix;
 }
 
 function main() {
