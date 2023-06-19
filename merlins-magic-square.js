4d3884573c76ec10f5288adf10223aa1e754db45
Khaled KHEBBEB <khaled.khebbeb@gmail.com>
2023-06-19 23:16:26 +0200

    Update merlins-magic-square.js

--- merlins-magic-square.js
+++ merlins-magic-square.js
@@ -1,26 +1,51 @@
-function input() {
+// get inputs
+function input(): array {
     var matrix = [readline(), readline(), readline()];
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            if (matrix[i][j] == "~") {
-                matrix[i][j] = 0;
-            }
-            else {
-                matrix[i][j] = 1;
-            }
-        }
-    }
     const buttons = readline();
-    return matrix, buttons;
+    return [matrix, buttons];
 }
 
-function reverse(button: int):int {
-    return (button+1)%2;
+// reverse a position
+function reverse(button: char):char {
+    return button == '*' ? '~' : '*';
 }
 
+// reverse a row (3 buttons)
+function reverse_row(row: array): array{
+    for (i = 0; i < 2; i++) {
+        row[i] = reverse(row[i]);
+    }
+    return row;
+}
+
+// reverse a square (2 x 2 buttons)
+function reverse_square(square: array): array{
+    for (i = 0; i < 2; i++) {
+        square[i] = reverse_row(square[i]);
+    }
+    return square;
+}
+
+function transform(matrix: array, button:char):array {
+    let button = parseInt(button);
+
+    if (button == 5) {
+        matrix[1][1] = reverse(matrix[1][1]);
+    }
+    return matrix;
+    
+    if (button in [1, 3, 7, 9]) {
+        
+    }
+    
+    if (button in [2, 4, 6, 8]) {
+
+    }
+    
+}
 
 function main() {
-    var matrix, buttons = input();
+    var [matrix, buttons] = input();
     
     var answer = null;
 
